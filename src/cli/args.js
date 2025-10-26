f8868b6f460ce2db119b3c36fd0f00518eeeda10
nikitakonan <nikitakonan1991@gmail.com>
2025-10-26 17:09:38 +0400

    feat: implement CLI argument and environment variable parsing

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv
+    .slice(2)
+    .reduce((acc, arg, index, array) => {
+      if (arg.startsWith('--')) {
+        const key = arg.slice(2);
+        const value = array[index + 1];
+        acc.push([key, value]);
+      }
+      return acc;
+    }, [])
+    .map(([key, value]) => `${key} is ${value}`)
+    .join(', ');
+  console.log(args);
 };
 
 parseArgs();
